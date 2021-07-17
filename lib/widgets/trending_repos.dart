a349e04e477bd79369ecdc2ed787bc625a22e7bd
Reuben Turner <groovinchip@gmail.com>
2021-07-17 15:09:27 -0400

    open trending repo on tap or click

--- lib/widgets/trending_repos.dart
+++ lib/widgets/trending_repos.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gh_trend/gh_trend.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TrendingRepos extends StatelessWidget {
   const TrendingRepos({
@@ -30,32 +31,41 @@ class TrendingRepos extends StatelessWidget {
                     .replaceAll('#', '0xff')))
                 .withOpacity(0.25);
           }
-          return Card(
-            color: cardColor,
-            child: Column(
-              children: [
-                ListTile(
-                  title: Text(
-                      '${trendingRepos[index].owner}/${trendingRepos[index].repoName}'),
-                  subtitle: Text(trendingRepos[index].description),
-                  trailing: Text(trendingRepos[index].programmingLanguage),
-                ),
-                Row(
-                  children: [
-                    const SizedBox(width: 16),
-                    RepoDetailItem(
-                      icon: const Icon(Icons.star_border),
-                      label: '${trendingRepos[index].totalStars}',
-                    ),
-                    const SizedBox(width: 8),
-                    RepoDetailItem(
-                      icon: const Icon(MdiIcons.sourceFork),
-                      label: '${trendingRepos[index].totalForks}',
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 8),
-              ],
+          return InkWell(
+            onTap: () async {
+              if (await canLaunch(
+                  'https://github.com/${trendingRepos[index].owner}/${trendingRepos[index].repoName}')) {
+                await launch(
+                    'https://github.com/${trendingRepos[index].owner}/${trendingRepos[index].repoName}');
+              }
+            },
+            child: Card(
+              color: cardColor,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(
+                        '${trendingRepos[index].owner}/${trendingRepos[index].repoName}'),
+                    subtitle: Text(trendingRepos[index].description),
+                    trailing: Text(trendingRepos[index].programmingLanguage),
+                  ),
+                  Row(
+                    children: [
+                      const SizedBox(width: 16),
+                      RepoDetailItem(
+                        icon: const Icon(Icons.star_border),
+                        label: '${trendingRepos[index].totalStars}',
+                      ),
+                      const SizedBox(width: 8),
+                      RepoDetailItem(
+                        icon: const Icon(MdiIcons.sourceFork),
+                        label: '${trendingRepos[index].totalForks}',
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                ],
+              ),
             ),
           );
         },
