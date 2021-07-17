31af2c0cf3af57cc9e4af18c2086577046cacd24
Reuben Turner <groovinchip@gmail.com>
2021-07-17 14:04:51 -0400

    Improve event card widget
    Removes expansion tile in favor of one card and adds a section for the type of event

--- lib/widgets/activity_list.dart
+++ lib/widgets/activity_list.dart
@@ -36,68 +36,79 @@ class _ActivityListState extends State<ActivityList> {
       delegate: SliverChildBuilderDelegate(
         (context, idx) {
           final event = _events[idx];
-          return Card(
-            color: Theme.of(context).brightness == Brightness.dark
-                ? themeDataDark.cardColor
-                : themeDataLight.cardColor,
-            child: ExpansionTile(
-              tilePadding:
-                  const EdgeInsets.symmetric(vertical: 6, horizontal: 10) +
-                      const EdgeInsets.only(left: 12, right: 4),
-              leading: UserAvatar(
-                avatarUrl: event.actor!.avatarUrl!,
-                username: event.actor!.login!,
-              ),
-              title: RichText(
-                text: TextSpan(
-                  children: [
-                    const WidgetSpan(
-                      child: Icon(Icons.chevron_right),
-                      alignment: PlaceholderAlignment.bottom,
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              color: Theme.of(context).brightness == Brightness.dark
+                  ? themeDataDark.cardColor
+                  : themeDataLight.cardColor,
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: UserAvatar(
+                      avatarUrl: event.actor!.avatarUrl!,
+                      username: event.actor!.login!,
                     ),
-                    WidgetSpan(
-                      child: Icon(
-                        event.payload!['action'].toString() == 'started'
-                            ? Icons.star
-                            : Icons.help,
+                    title: RichText(
+                      text: TextSpan(
+                        children: [
+                          const WidgetSpan(
+                            child: Icon(Icons.chevron_right),
+                            alignment: PlaceholderAlignment.bottom,
+                          ),
+                          WidgetSpan(
+                            child: Icon(
+                              event.payload!['action'].toString() == 'started'
+                                  ? Icons.star
+                                  : Icons.help,
+                            ),
+                          ),
+                          const WidgetSpan(
+                            child: Icon(Icons.chevron_right),
+                            alignment: PlaceholderAlignment.bottom,
+                          ),
+                          TextSpan(
+                            text: event.repo!.name.substring(
+                              0,
+                              event.repo!.name.indexOf('/'),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    const WidgetSpan(
-                      child: Icon(Icons.chevron_right),
-                      alignment: PlaceholderAlignment.bottom,
-                    ),
-                    TextSpan(
-                      text: event.repo!.name.substring(
-                            0,
-                            event.repo!.name.indexOf('/'),
-                          ),
-                    ),
-                  ],
-                ),
-              ),
-              trailing: CreatedAt(
-                timeCreated: event.createdAt!,
-              ),
-              children: [
-                Card(
-                  color: Colors.transparent,
-                  child: ListTile(
-                    leading: const Text('User'),
-                    title: Text(
-                      event.actor!.login!,
+                    trailing: CreatedAt(
+                      timeCreated: event.createdAt!,
                     ),
                   ),
-                ),
-                Card(
-                  color: Colors.transparent,
-                  child: ListTile(
-                    leading: const Text('Repo'),
-                    title: Text(
-                      event.repo!.name,
+                  Card(
+                    color: Colors.transparent,
+                    child: ListTile(
+                      leading: const Text('User'),
+                      title: Text(
+                        event.actor!.login!,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                  Card(
+                    color: Colors.transparent,
+                    child: ListTile(
+                      leading: const Text('Repo'),
+                      title: Text(
+                        event.repo!.name,
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Colors.transparent,
+                    child: ListTile(
+                      leading: const Text('Type'),
+                      title: Text(
+                        event.type!,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
