9b31d1087bd15e913a92233ca4fed457211427a4
Reuben Turner <groovinchip@gmail.com>
2021-07-19 10:01:25 -0400

    more repo detail progress

--- lib/widgets/repo_preview.dart
+++ lib/widgets/repo_preview.dart
@@ -3,6 +3,7 @@ import 'package:gaf/providers.dart';
 import 'package:gaf/widgets/user_avatar.dart';
 import 'package:github/github.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class RepoPreview extends HookConsumerWidget {
@@ -50,9 +51,56 @@ class RepoPreview extends HookConsumerWidget {
             ),
             const SizedBox(height: 8),
             Text(repo.description),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                RepoDetailItem(
+                  icon: const Icon(Icons.remove_red_eye_outlined),
+                  label: '${repo.watchersCount}',
+                ),
+                const SizedBox(width: 12),
+                RepoDetailItem(
+                  icon: const Icon(Icons.star_border),
+                  label: '${repo.stargazersCount}',
+                ),
+                const SizedBox(width: 12),
+                RepoDetailItem(
+                  icon: const Icon(MdiIcons.sourceFork),
+                  label: '${repo.forksCount}',
+                ),
+                const SizedBox(width: 12),
+                RepoDetailItem(
+                  icon: const Icon(Icons.info_outline),
+                  label: '${repo.openIssuesCount}',
+                ),
+              ],
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class RepoDetailItem extends StatelessWidget {
+  const RepoDetailItem({
+    Key? key,
+    required this.icon,
+    required this.label,
+  }) : super(key: key);
+
+  final Widget icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        icon,
+        const SizedBox(width: 8),
+        Text(label),
+      ],
     );
   }
 }
