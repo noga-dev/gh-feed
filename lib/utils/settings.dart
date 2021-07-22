60c40c6edf7f9e98f1f5ad682842c4b9cfc31f8b
groovinchip <groovinchip@gmail.com>
2021-07-22 11:59:00 -0400

    Add another settings property

--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -1,21 +1,29 @@
 class Settings {
   Settings({
     this.filterPushEvents = false,
+    this.filterDeleteEvents = false,
   });
 
   factory Settings.fromJson(Map<dynamic, dynamic> json) => Settings(
-        filterPushEvents: json['filterPushEvents'],
+        filterPushEvents: json['filterPushEvents'] ?? false,
+        filterDeleteEvents: json['filterDeleteEvents'] ?? false,
       );
 
-  Settings copyWith({bool? filterPushEvents}) {
+  Settings copyWith({
+    bool? filterPushEvents,
+    bool? filterDeleteEvents,
+  }) {
     return Settings(
       filterPushEvents: filterPushEvents ?? this.filterPushEvents,
+      filterDeleteEvents: filterDeleteEvents ?? this.filterDeleteEvents,
     );
   }
 
   final bool filterPushEvents;
+  final bool filterDeleteEvents;
 
   Map<String, dynamic> toJson() => {
         'filterPushEvents': filterPushEvents,
+        'filterDeleteEvents': filterDeleteEvents,
       };
 }
